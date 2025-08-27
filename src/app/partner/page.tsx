9421f065595a859a9bd41f5a3326cb07b45f1628
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-08-27 18:45:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/partner/page.tsx
+++ src/app/partner/page.tsx
@@ -581,7 +581,7 @@ export default function PartnerPage() {
                             )} />
                          </div>
                          <FormField control={individualUploadForm.control} name="cvDocument" render={({ field }) => (
-                            <FormItem><FormLabel>CV / Resume / Portfolio (Optional)</FormLabel><FormControl><Input type="file" accept=".pdf,.png,.jpg,.jpeg" onChange={(e) => field.onChange(e.target.files)} /></FormControl><FormMessage /></FormItem>
+                            <FormItem><FormLabel>CV / Resume / Portfolio (Optional)</FormLabel><FormControl><Input type="file" accept=".pdf,.docx,.doc" onChange={(e) => field.onChange(e.target.files)} /></FormControl><FormMessage /></FormItem>
                         )} />
                         <FormField
                             control={individualUploadForm.control}
@@ -820,7 +820,7 @@ export default function PartnerPage() {
         case 'capture_id_front': return <CameraCapture title="Scan Front of ID Card" onCapture={onIdFrontCaptured} onCancel={() => setPageState('upload')} />;
         case 'capture_id_back': return <CameraCapture title="Scan Back of ID Card" onCapture={onIdBackCaptured} onCancel={() => setPageState('upload')} />;
         case 'capture_rep_id_front': return <CameraCapture title="Scan Representative's ID Front" onCapture={onRepIdFrontCaptured} onCancel={() => setPageState('upload')} />;
-        case 'capture_rep_id_back': return <CameraCapture title="Scan Representative's ID Back" onRepIdBackCaptured} onCancel={() => setPageState('upload')} />;
+        case 'capture_rep_id_back': return <CameraCapture title="Scan Representative's ID Back" onCapture={onRepIdBackCaptured} onCancel={() => setPageState('upload')} />;
         default: return <SelectionScreen />;
     }
   };
