163253db07a07cc2e48e5a172a456eb70b3baaaf
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-08-27 06:56:24 +0000

    enreach all template files with dropdown list as posible to easy filling

--- src/app/cctv-estimator/quotation-form.tsx
+++ src/app/cctv-estimator/quotation-form.tsx
@@ -578,7 +578,6 @@ export default function QuotationForm() {
                  </div>
             </Card>
 
-
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                  <FormField
                     control={form.control}
