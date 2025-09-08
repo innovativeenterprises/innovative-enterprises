1a572cb6f11378eb8c6bba8b7508837b8e82bff6
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-08 11:09:49 +0000

    continue 404 not solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   }, []);
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased',
