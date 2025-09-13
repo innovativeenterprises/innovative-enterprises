84276a0c1a8346d7072a402b9b394eebf0701068
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-13 19:28:37 +0000

    continue

--- src/app/admin/finance/page.tsx
+++ src/app/admin/finance/page.tsx
@@ -2,11 +2,9 @@
 'use client';
 
 import InvestorTable from "../investor-table";
-import { useInvestorsData } from "@/hooks/use-global-store-data";
 import CfoDashboard from "../cfo-dashboard";
 
 export default function AdminFinancePage() {
-  const { investors, isClient } = useInvestorsData();
 
   return (
     <div className="space-y-8">
