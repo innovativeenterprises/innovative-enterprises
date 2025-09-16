843f5754037de28b662858e419ee3780bca4ff4d
Anwar Ahmed Sharif <anwarahmedsharif@gmail.com>
2025-09-16 15:31:10 +0000

    Fix parent homepage, don't stop till fix it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <head />
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
         <ClientLayout>{children}</ClientLayout>
