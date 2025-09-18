733ad7efe23bde51f744552c44ac1045ca356c13
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-18 01:50:31 +0000

    blank homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.variable)}>
         <ClientLayout>{children}</ClientLayout>
       </body>
