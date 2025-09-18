0a4d0da17921b03af7c112e10e785423f1d0c052
Anwar Ahmed Sharif <anwarahmedsharif@gmail.com>
2025-09-18 23:02:39 +0000

    No header nor footer too

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -4,11 +4,8 @@
 import { Toaster } from '@/components/ui/toaster';
 import ChatWidget from '@/components/chat-widget';
 import { StoreProvider } from '@/components/layout/store-provider';
-import Header from '@/components/layout/header';
-import Footer from '@/components/layout/footer';
 import type { AppState } from '@/lib/global-store';
 import { ThemeProvider } from 'next-themes';
-import { usePathname } from 'next/navigation';
 
 export default function ClientLayout({
   children,
@@ -17,9 +14,6 @@ export default function ClientLayout({
   children: React.ReactNode;
   initialData: Partial<AppState>;
 }) {
-  const pathname = usePathname();
-  const isHomePage = pathname === '/';
-
   return (
     <ThemeProvider
         attribute="class"
@@ -29,9 +23,7 @@ export default function ClientLayout({
     >
         <StoreProvider initialData={initialData}>
         <div className="flex min-h-screen flex-col">
-            {!isHomePage && <Header />}
             <main className="flex-1">{children}</main>
-            {!isHomePage && <Footer />}
             <Toaster />
             <ChatWidget />
         </div>
