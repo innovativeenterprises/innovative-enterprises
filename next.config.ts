f39fb2ca88facf83d7e2fd083a2b8d60aa2bd80c
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-18 10:39:52 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'innovative-enterprises.firebasestorage.app',
+      },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
