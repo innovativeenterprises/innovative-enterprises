65caad8f6e3638d7a08a959ec9a26b99de131c7c
Anwar Ahmed Sharif <anwarahmedsharif@gmail.com>
2025-09-20 11:04:03 +0000

    Continue

--- src/app/community-hub/directory/page.tsx
+++ src/app/community-hub/directory/page.tsx
@@ -4,6 +4,6 @@ import MemberDirectoryClient from './client-page';
 import { useMembersData } from '@/hooks/use-global-store-data';
 
 export default function MemberDirectoryPage() {
-    const { members } = useMembersData();
-    return <MemberDirectoryClient initialMembers={members} />;
+    const { members, isClient } = useMembersData();
+    return <MemberDirectoryClient initialMembers={isClient ? members : []} />;
 }
