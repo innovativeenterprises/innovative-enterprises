7ec0a7ba8e6beadfdca7fb116e06fc9dde55ad53
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-21 18:52:23 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,15 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // This is a workaround for a build error related to the handlebars dependency.
+      // It can be removed if the dependency is updated.
+      'handlebars': 'handlebars/dist/handlebars.js',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
