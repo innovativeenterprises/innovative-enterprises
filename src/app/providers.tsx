34e818c0b231b96de21598081b3e8f2ffb80f569
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-22 19:08:39 +0000

    nothing in homepage got only 404

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,11 +5,7 @@ import React, { ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from '@/components/ui/toaster';
 
-export function Providers({ 
-    children,
-}: { 
-    children: ReactNode,
-}) {
+export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
