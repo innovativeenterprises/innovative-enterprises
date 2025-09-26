4a4732ad9007fbcfa74924955cef5f96d2b88cd3
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-26 19:20:50 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://upload.

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'innovative-enterprises.firebasestorage.app',
       },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
