0148ae010d51f2eb3e8931d49ed4562bc672e51f
Anwar Ahmed Sharif <anwarahmedsharif@gmail.com>
2025-09-26 17:51:23 +0000

    Search and update original partner logos

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -2,11 +2,11 @@
 import type { Client, Testimonial } from './clients.schema';
 
 export const initialClients: Client[] = [
-  { id: '1', name: 'OmanTel', logo: 'https://placehold.co/150x60/293462/F0F4F8?text=OmanTel', aiHint: 'telecom logo' },
-  { id: '2', name: 'Petroleum Development Oman', logo: 'https://placehold.co/150x60/293462/F0F4F8?text=PDO', aiHint: 'oil company logo' },
-  { id: '3', name: 'Bank Muscat', logo: 'https://placehold.co/150x60/293462/F0F4F8?text=Bank+Muscat', aiHint: 'bank logo' },
-  { id: '4', name: 'Omran', logo: 'https://placehold.co/150x60/293462/F0F4F8?text=Omran', aiHint: 'tourism logo' },
-  { id: '5', name: 'Sohar Port and Freezone', logo: 'https://placehold.co/150x60/293462/F0F4F8?text=Sohar+Port', aiHint: 'port logo' },
+  { id: '1', name: 'OmanTel', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Omantel_Logo.svg/1024px-Omantel_Logo.svg.png', aiHint: 'telecom logo' },
+  { id: '2', name: 'Petroleum Development Oman', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Petroleum_Development_Oman_logo.svg/320px-Petroleum_Development_Oman_logo.svg.png', aiHint: 'oil company logo' },
+  { id: '3', name: 'Bank Muscat', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bank_Muscat_Logo.svg/320px-Bank_Muscat_Logo.svg.png', aiHint: 'bank logo' },
+  { id: '4', name: 'Omran', logo: 'https://www.omran.om/images/logo-en.png', aiHint: 'tourism logo' },
+  { id: '5', name: 'Sohar Port and Freezone', logo: 'https://www.soharportandfreezone.com/sites/default/files/2019-10/logo.png', aiHint: 'port logo' },
 ];
 
 export const initialTestimonials: Testimonial[] = [
