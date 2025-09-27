940b08749f81698aeef90c847d5ee25faae57565
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-27 08:41:15 +0000

    enable locale json files (English and Arabic)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
