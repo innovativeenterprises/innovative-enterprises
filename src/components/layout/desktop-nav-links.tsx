0b8cb6d67b074e06b2ae9836ecebbe2be305df18
Anwar Ahmed Sharif <anwarahmedsharif@gmail.com>
2025-09-27 16:35:31 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/desktop-nav-links.tsx
+++ src/components/layout/desktop-nav-links.tsx
@@ -22,7 +22,7 @@ export default function DesktopNavLinks() {
     <>
       {navLinks.map((link) => (
         <NavigationMenuItem key={link.href}>
-          <Link href={link.href} legacyBehavior passHref>
+          <Link href={link.href} legacyBehavior={false} passHref>
             <NavigationMenuLink active={pathname === link.href} className={cn(navigationMenuTriggerStyle(), 'text-base font-medium')}>
                 {link.label}
             </NavigationMenuLink>
