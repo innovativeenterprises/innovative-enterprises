f78070ebb394d283fab5c7998353cbb28367543a
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-27 18:55:42 +0000

    Continue

--- src/app/components/layout/header-client.tsx
+++ src/app/components/layout/header-client.tsx
@@ -38,7 +38,7 @@ import { useCartData, useAiToolsData, useSolutionsData, useIndustriesData, useSe
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a"> & { iconName: string; title: string }
+  React.ComponentPropsWithoutRef<"a"> & { iconName: string; }
 >(({ className, title, children, iconName, ...props }, ref) => {
   const Icon = (Icons as any)[iconName] || Icons.HelpCircle;
   return (
@@ -116,14 +116,14 @@ export default function HeaderClient() {
                 <NavigationMenuContent>
                   <ul className={cn("grid w-[400px] gap-3 p-4", settings && settings.servicesMenuColumns === 2 && "md:w-[500px] md:grid-cols-2", settings && settings.servicesMenuColumns >= 3 && "md:w-[600px] md:grid-cols-3")}>
                     {(solutions || []).map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                        iconName={component.icon}
-                      >
-                        {component.description}
-                      </ListItem>
+                      <Link href={component.href} key={component.title} legacyBehavior passHref>
+                          <ListItem
+                            title={component.title}
+                            iconName={component.icon}
+                          >
+                            {component.description}
+                          </ListItem>
+                        </Link>
                     ))}
                   </ul>
                 </NavigationMenuContent>
@@ -133,14 +133,14 @@ export default function HeaderClient() {
                 <NavigationMenuContent>
                   <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
                     {(industries || []).map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                        iconName={component.icon}
-                      >
-                        {component.description}
-                      </ListItem>
+                      <Link href={component.href} key={component.title} legacyBehavior passHref>
+                          <ListItem
+                            title={component.title}
+                            iconName={component.icon}
+                          >
+                            {component.description}
+                          </ListItem>
+                        </Link>
                     ))}
                   </ul>
                 </NavigationMenuContent>
@@ -150,14 +150,14 @@ export default function HeaderClient() {
                 <NavigationMenuContent>
                   <ul className={cn("grid w-[400px] gap-3 p-4", settings && settings.aiToolsMenuColumns === 2 && "md:w-[500px] md:grid-cols-2", settings && settings.aiToolsMenuColumns >= 3 && "md:w-[600px] md:grid-cols-3", settings && settings.aiToolsMenuColumns >= 4 && "lg:w-[800px] lg:grid-cols-4")}>
                     {(aiTools || []).map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                        iconName={component.icon}
-                      >
-                        {component.description}
-                      </ListItem>
+                      <Link href={component.href} key={component.title} legacyBehavior passHref>
+                          <ListItem
+                            title={component.title}
+                            iconName={component.icon}
+                          >
+                            {component.description}
+                          </ListItem>
+                        </Link>
                     ))}
                   </ul>
                 </NavigationMenuContent>
