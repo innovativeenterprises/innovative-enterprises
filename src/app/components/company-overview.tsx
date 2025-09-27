a4265fb1d398e75f15e139c19887b125fcaf8a28
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-27 09:00:04 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/components/company-overview.tsx
+++ src/app/components/company-overview.tsx
@@ -1,9 +1,7 @@
-
 'use client';
 
 import CompanyOverviewClient from './company-overview-client';
 
 export default function CompanyOverview() {
-  // The client component now fetches its own data.
   return <CompanyOverviewClient />;
 }
