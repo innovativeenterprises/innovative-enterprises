0c0e15402fd7f77d55e3ec4930d73970358a2c75
Innovative Enterprises <innovativeenterprisesoman@gmail.com>
2025-09-28 01:19:09 +0000

    continue fix all also check all hyberlinks functionality

--- src/app/lib/global-store.ts
+++ src/app/lib/global-store.ts
@@ -1,8 +1,8 @@
 
 'use client';
 
-import { createStore, useStore as useZustandStore } from 'zustand';
 import React, { createContext, useContext, ReactNode, useRef } from 'react';
+import { createStore, useStore as useZustandStore } from 'zustand';
 import type { AppState } from './initial-state';
 import { getInitialState } from './initial-state';
 
@@ -10,12 +10,12 @@ export type AppStore = AppState & {
   set: (updater: (state: AppState) => Partial<AppState>) => void;
 };
 
-export type StoreType = ReturnType<typeof createAppStore>;
-
 export const createAppStore = (initState: Partial<AppState> = {}) => {
   const initialState = { ...getInitialState(), ...initState };
-  return createStore<AppStore>()((set) => ({
+  return createStore<AppStore>((set) => ({
     ...initialState,
     set: (updater) => set(updater),
   }));
 };
+
+export type StoreType = ReturnType<typeof createAppStore>;
